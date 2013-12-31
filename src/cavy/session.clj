96bfd5751c3b6650dd00550bc89c8901eea8d32c
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-12-31 18:42:02 -0500

    Adding generic cookie store.

--- src/cavy/session.clj
+++ src/cavy/session.clj
@@ -1,13 +1,15 @@
 (ns cavy.session
   "The core Cavy session state."
-  (:require [clj-http.client :as http]))
+  (:require [clj-http.client :as http]
+            [cavy.cookies :as cookies]))
 
 (defn create
   "Creates a new session."
   [& options]
   (let [options (apply hash-map options)
+        cookies (or (:cookies options) (cookies/mem-store))
         location (:url options)]
     {:options options
-     :cookies nil
+     :cookies cookies
      :location location
      :page nil}))
