a30f489d844e87f352599a5c0a4c1b333a9cf25f
Nathan Clark <Nathan.Clark@tokenshift.com>
2014-01-01 15:43:35 -0500

    Updating project description and license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject cavy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject cavy "0.0.1-SNAPSHOT"
+  :description "Clojure HTTP client for UI automation and testing."
+  :url "https://github.com/tokenshift/cavy"
+  :license {:name "The MIT License (MIT)"
+            :url "https://raw.github.com/tokenshift/cavy/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.8"]
                  [com.cemerick/url "0.1.0"]])
