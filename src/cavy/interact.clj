23edf53b229ce381bed838676dcd2d8a1267ed90
Nathan Clark <Nathan.Clark@nasa.gov>
2014-01-02 14:44:20 -0500

    Added tests for set-value.

--- src/cavy/interact.clj
+++ src/cavy/interact.clj
@@ -1,3 +1,8 @@
 (ns cavy.interact
   "Functions for interacting with a Cavy session."
   (:require [cavy.query :as query]))
+
+(defn set-value
+  "Sets the text value of an input field."
+  [page target text]
+  page)
--- test/cavy/test/unit/interact.clj
+++ test/cavy/test/unit/interact.clj
@@ -1,3 +1,21 @@
-(ns cavy.test.unit.interact)
+(ns cavy.test.unit.interact
+  (:require [net.cgrand.enlive-html :as enlive]
+            [cavy.interact :as interact])
+  (:use clojure.test))
 
+(def page
+  (enlive/html-snippet (slurp "test/cavy/test/test-pages/query-tests.html")))
 
+(deftest set-value
+  (testing "by label"
+    (let [result (interact/set-value page "Field 1" "Testing")
+          field (enlive/select result [:#field1])
+          value (get-in field [:attrs :value])]
+      (is (= "Testing" value))))
+  (testing "by selector"
+    (let [result (interact/set-value page
+                                     [[:input (enlive/attr= :aria-label "Field 3")]]
+                                     "Testing 2")
+          field (enlive/select result [:#field1])
+          value (get-in field [:attrs :value])]
+      (is (= "Testing 2" value)))))
