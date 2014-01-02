52466265f66919453f3a41c151d05d5d987521dd
Nathan Clark <Nathan.Clark@nasa.gov>
2014-01-02 13:26:29 -0500

    Fixing protocol-relative links.

--- project.clj
+++ project.clj
@@ -3,8 +3,6 @@
   :url "https://github.com/tokenshift/cavy"
   :license {:name "The MIT License (MIT)"
             :url "https://raw.github.com/tokenshift/cavy/master/LICENSE"}
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.0.0"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.8"]
                  [clojurewerkz/urly "1.0.0"]
