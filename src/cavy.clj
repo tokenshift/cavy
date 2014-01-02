ac1d3464ff5db01b0e08e966f86513cc6aeaee66
Nathan Clark <Nathan.Clark@nasa.gov>
2014-01-02 10:44:23 -0500

    Renaming some API functions.

--- src/cavy.clj
+++ src/cavy.clj
@@ -8,44 +8,39 @@
   [& options]
   (apply session/create options))
 
-(defn click-button
-  "Clicks on a button."
-  [session name]
-  session)
-
-(defn click-link
-  "Clicks on a link."
-  [session name]
+(defn press
+  "Presses a button."
+  [session target]
   session)
 
 (defn click
-  "Clicks on a link or button."
-  [session name]
+  "Clicks on a link."
+  [session target]
   session)
 
 (defn fill-in
   "Fills in the specified input field."
-  [session label value]
+  [session target value]
   session)
 
 (defn check
   "Checks a checkbox."
-  [session name]
+  [session target]
   session)
 
 (defn uncheck
   "Unchecks a checkbox."
-  [session name]
+  [session target]
   session)
 
 (defn toggle
   "Toggles the checkbox state."
-  [session name]
+  [session target]
   session)
 
 (defn select
   "Selects options in a radio group or dropdown."
-  [session name & values]
+  [session target & values]
   session)
 
 (defn visit
