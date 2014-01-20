9a5f1b2d101e1ed20b39a619a8c4f4d764cc3930
Nathan Clark <Nathan.Clark@nasa.gov>
2014-01-20 14:25:01 -0500

    Moving negative tests to unit tests.

--- test/cavy/test/functional/api.clj
+++ test/cavy/test/functional/api.clj
@@ -4,26 +4,10 @@
   (:use clojure.test
         cavy.test.functional.session))
 
-(defn check-session
-  "Runs a predicate against the session."
-  [session pred]
-  (is (pred session))
-  session)
-
 (defmacro with-session
   "Helper to thread a session through an arbitrary form."
   [session name & forms]
   `((fn [~name] ~@forms ~name) ~session))
-
-(defmacro does-nothing
-  "Verifies that the specified action(s) does not provoke navigation."
-  [session & forms]
-  `((fn [s#]
-      (-> s#
-          (assoc :location ::did-nothing)
-          ~@forms
-          (check-session #(= ::did-nothing (:location %1)))))
-      ~session))
 
 (defn went-to
   "Verifies that the session has navigated to the specified URL."
@@ -55,10 +39,7 @@
   (testing "with a fully specified URL"
     (-> (test-session "http://example.com/link1.html")
         (cavy/click "external")
-        (went-to "http://example2.com/link.html")))
-  (testing "link not found"
-    (-> (test-session "http://example.com/link1.html")
-        (does-nothing (cavy/click "whatever")))))
+        (went-to "http://example2.com/link.html"))))
 
 (deftest fill-in
   (testing "login fields"
@@ -69,10 +50,7 @@
           username (first (enlive/select page [:#username]))
           password (first (enlive/select page [:#password]))]
       (is (= "test-user" (get-in username [:attrs :value])))
-      (is (= "test-password" (get-in password [:attrs :value])))))
-  (testing "field not found"
-    (-> (test-session "http://example.com/login.html")
-        (does-nothing (cavy/fill-in "Whatever" "testing")))))
+      (is (= "test-password" (get-in password [:attrs :value]))))))
 
 (deftest press
   (testing "submitting login form"
@@ -86,15 +64,4 @@
       (is (= "http://example.com/login" (request :url)))
       (is (not (nil? (get-in request [:options :form-params]))))
       (is (= ["test-username"] (get-in request [:options :form-params :username])))
-      (is (= ["test-password"] (get-in request [:options :form-params :password])))))
-  (testing "submit button with no value"
-    (let [result (-> (test-session "http://example.com/form.html")
-                     (cavy/fill-in "Field 1" "testing")
-                     (cavy/press "Submit"))
-          request @(result :last-request)]
-      (is (= :post (request :method)))
-      (is (= "http://example.com/test-submit" (request :url)))
-      (is (= ["testing"] (-> request :options :form-params :field1)))))
-  (testing "button not found"
-    (-> (test-session "http://example.com/form.html")
-        (does-nothing (cavy/press "whatever")))))
+      (is (= ["test-password"] (get-in request [:options :form-params :password]))))))
