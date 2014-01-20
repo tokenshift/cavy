7cee593fd66baf56ecf423ead7c9106cb0824100
Nathan Clark <Nathan.Clark@nasa.gov>
2014-01-20 14:25:01 -0500

    Moving negative tests to unit tests.

--- test/cavy/test/unit/interact.clj
+++ test/cavy/test/unit/interact.clj
@@ -3,35 +3,58 @@
             [cavy.interact :as interact])
   (:use clojure.test))
 
-(def page
+(defn unchanged
+  "Verifies that two versions of the page are identical."
+  [el1 el2]
+  (cond
+    (instance? clojure.lang.Counted el1)
+    (if (= (count el1) (count el2))
+      (every? identity (map unchanged el1 el2))
+      false)
+
+    (map? el1)
+    (if (map? el2)
+      (every? identity (map (fn [[k v]] (= v (el2 k))) el1))
+      false)
+
+    :else
+    (= el1 el2)))
+
+(def query-tests
   (enlive/html-snippet (slurp "test/cavy/test/test-pages/query-tests.html")))
 
 (deftest set-checked
   (testing "checked = true"
-    (let [result (interact/set-checked page "Field 5" true)
+    (let [result (interact/set-checked query-tests "Field 5" true)
           field (first (enlive/select result [:#field5]))]
       (is (= "checked" (get-in field [:attrs :checked])))))
   (testing "checked = false"
-    (let [result (interact/set-checked page "Field 5" false)
+    (let [result (interact/set-checked query-tests "Field 5" false)
           field (first (enlive/select result [:#field5]))]
       (is (nil? (get-in field [:attrs :checked])))
       (is (not (contains? (field :attrs) :checked)))))
   (testing "checked = toggle"
-    (let [result1 (interact/set-checked page "Field 5" :toggle)
+    (let [result1 (interact/set-checked query-tests "Field 5" :toggle)
           result2 (interact/set-checked result1 "Field 5" :toggle)
           field1 (first (enlive/select result1 [:#field5]))
           field2 (first (enlive/select result2 [:#field5]))]
       (is (= "checked" (get-in field1 [:attrs :checked])))
-      (is (nil? (get-in field2 [:attrs :checked]))))))
+      (is (nil? (get-in field2 [:attrs :checked])))))
+  (testing "checkbox not found"
+    (let [result (interact/set-checked query-tests "Whatever" :toggle)]
+      (is (unchanged result query-tests)))))
 
 (deftest set-field-value
   (testing "by label"
-    (let [result (interact/set-field-value page "Field 1" "Testing")
+    (let [result (interact/set-field-value query-tests "Field 1" "Testing")
           field (first (enlive/select result [:#field1]))
           value (get-in field [:attrs :value])]
       (is (= "Testing" value))))
   (testing "by selector"
-    (let [result (interact/set-field-value page [:#field1] "Testing 2")
+    (let [result (interact/set-field-value query-tests [:#field1] "Testing 2")
           field (first (enlive/select result [:#field1]))
           value (get-in field [:attrs :value])]
-      (is (= "Testing 2" value)))))
+      (is (= "Testing 2" value))))
+  (testing "field not found"
+    (let [result (interact/set-field-value query-tests "Whatever" "Testing")]
+      (is (unchanged result query-tests)))))
--- test/cavy/test/unit/query.clj
+++ test/cavy/test/unit/query.clj
@@ -3,7 +3,7 @@
             [cavy.query :as query])
   (:use [clojure.test]))
 
-(def page
+(def query-tests
   (enlive/html-snippet (slurp "test/cavy/test/test-pages/query-tests.html")))
 
 (def test-form
@@ -11,6 +11,9 @@
 
 (def test-form-filled
   (enlive/html-snippet (slurp "test/cavy/test/test-pages/login-filled.html")))
+
+(def form
+  (enlive/html-snippet (slurp "test/cavy/test/test-pages/form.html")))
 
 (deftest find-button
   (testing "by text"
@@ -20,7 +23,12 @@
   (testing "by selector"
     (let [button (query/find-button test-form [(enlive/attr= :type "submit")])]
       (is (not (nil? button)))
-      (is (= "Login" (get-in button [:attrs :value]))))))
+      (is (= "Login" (get-in button [:attrs :value])))))
+  (testing "with no value"
+    (let [button (query/find-button form "Submit")]
+      (is (not (nil? button))))
+    (let [button (query/find-button form "Click")]
+      (is (nil? button)))))
 
 (deftest find-form-with-button
   (let [[form button] (query/find-form-with-button test-form "Login")]
@@ -31,35 +39,35 @@
 
 (deftest find-label
   (testing "without colon"
-    (let [label (query/find-label page "Field 1")]
+    (let [label (query/find-label query-tests "Field 1")]
       (is (= "field1" (get-in label [:attrs :for])))))
   (testing "with colon"
-    (let [label (query/find-label page "Field 2")]
+    (let [label (query/find-label query-tests "Field 2")]
       (is (= "label2" (get-in label [:attrs :id])))))
   (testing "with html in label"
-    (let [label (query/find-label page "Field 4")]
+    (let [label (query/find-label query-tests "Field 4")]
       (is (= "field4" (get-in label [:attrs :for]))))))
 
 (deftest find-by-label
   (testing "label for"
-    (let [field (query/find-by-label page "Field 1")]
+    (let [field (query/find-by-label query-tests "Field 1")]
       (is (= "field1" (get-in field [:attrs :id])))))
   (testing "labelled by"
-    (let [field (query/find-by-label page "Field 2")]
+    (let [field (query/find-by-label query-tests "Field 2")]
       (is (= "label2" (get-in field [:attrs :aria-labelledby])))))
   (testing "label"
-    (let [field (query/find-by-label page "Field 3")]
+    (let [field (query/find-by-label query-tests "Field 3")]
       (is (= "Field 3" (get-in field [:attrs :aria-label]))))))
 
 (deftest find-link
   (testing "by text"
-    (let [link (query/find-link page "Link 4")]
+    (let [link (query/find-link query-tests "Link 4")]
       (is (= "test4.html" (get-in link [:attrs :href])))))
   (testing "by href"
-    (let [link (query/find-link page "test2.html")]
+    (let [link (query/find-link query-tests "test2.html")]
       (is (= "test2.html" (get-in link [:attrs :href])))))
   (testing "by enlive selector")
-    (let [link (query/find-link page [:ul :li :a.foo])]
+    (let [link (query/find-link query-tests [:ul :li :a.foo])]
       (is (= "test3.html" (get-in link [:attrs :href])))))
 
 (deftest get-form-method
